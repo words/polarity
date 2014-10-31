683c792ef563da411183e6b57c922df8c10733ad
wooorm <tituswormer@gmail.com>
2014-10-31 12:28:38 +0100

    Refactor module

--- index.js
+++ index.js
@@ -1,37 +1,71 @@
 'use strict';
 
-var afinn, isOwnProperty;
+/**
+ * Dependencies.
+ */
+
+var afinn;
 
 afinn = require('afinn-111');
 
-isOwnProperty = Object.prototype.hasOwnProperty;
+/**
+ * Cached methods.
+ */
+
+var has;
+
+has = Object.prototype.hasOwnProperty;
+
+/**
+ * Get the polarity of a word.
+ *
+ * @param {string} value
+ * @param {Object.<string, number>} inject
+ * @return {number}
+ */
 
 function getPolarity(value, inject) {
-    if (isOwnProperty.call(afinn, value)) {
+    if (has.call(afinn, value)) {
         return afinn[value];
-    } else if (inject && isOwnProperty.call(inject, value)) {
+    } else if (inject && has.call(inject, value)) {
         return inject[value];
     }
 
     return 0;
 }
 
+/**
+ * Define `polarity`
+ *
+ * @param {Array.<string>} values
+ * @param {Object.<string, number>} inject
+ * @return {Object}
+ */
+
 function polarity(values, inject) {
-    var iterator, length, value, weigth, positive, negative, positivity,
+    var index,
+        value,
+        weigth,
+        positive,
+        negative,
+        positivity,
         negativity;
 
     if (!values) {
         values = [];
     }
 
-    length = values.length || 0;
-    positivity = negativity = 0;
-    iterator = -1;
+    index = values.length || 1;
+
+    positivity = 0;
+    negativity = 0;
+
     positive = [];
     negative = [];
 
-    while (++iterator < length) {
-        value = values[iterator];
+    while (index--) {
+        value = values[index];
+
         weigth = getPolarity(value, inject);
 
         if (!weigth) {
@@ -40,9 +74,11 @@ function polarity(values, inject) {
 
         if (weigth > 0) {
             positive.push(value);
+
             positivity += weigth;
         } else {
             negative.push(value);
+
             negativity += weigth;
         }
     }
@@ -56,19 +92,37 @@ function polarity(values, inject) {
     };
 }
 
+/**
+ * Inject values on the `afinn` object.
+ *
+ * @param {Object.<string, number>} inject
+ */
+
 function inject(values) {
     var value;
 
     for (value in values) {
         /* istanbul ignore else */
-        if (isOwnProperty.call(values, value)) {
+        if (has.call(values, value)) {
             afinn[value] = values[value];
         }
     }
 }
 
+/**
+ * Expose `inject` on `polarity`.
+ */
+
 polarity.inject = inject;
+
+/**
+ * Expose `afinn` on `polarity`.
+ */
 
 polarity.afinn = afinn;
 
-exports = module.exports = polarity;
+/**
+ * Expose `polarity`.
+ */
+
+module.exports = polarity;
