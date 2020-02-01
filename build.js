4cc384b250d4b7b11d4a35574369b45a5a62ced6
Titus Wormer <tituswormer@gmail.com>
2020-02-01 10:38:54 +0100

    Update dev-dependencies

--- build.js
+++ build.js
@@ -2,13 +2,13 @@
 
 var fs = require('fs')
 var emotion = require('emoji-emotion')
-var gemoji = require('gemoji')
+var emojiToName = require('gemoji/emoji-to-name')
 
 var data = {}
 
 emotion.forEach(function(info) {
   data[info.emoji] = info.polarity
-  data[':' + gemoji.unicode[info.emoji].name + ':'] = info.polarity
+  data[':' + emojiToName[info.emoji] + ':'] = info.polarity
 })
 
 fs.writeFileSync('emoji.json', JSON.stringify(data, null, 2) + '\n')
