488b6ceff8b9fd656cd36e3ff9e930d9d5b4f19f
Titus Wormer <tituswormer@gmail.com>
2021-04-06 20:27:33 +0200

    Add JSDoc based types

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+/**
+ * @typedef {Object} Polarity
+ * @property {number} polarity
+ * @property {number} positivity
+ * @property {number} negativity
+ * @property {Array.<string>} positive
+ * @property {Array.<string>} negative
+ *
+ * @typedef {Object.<string, number>} Inject
+ */
+
 import {afinn165} from 'afinn-165'
 import {emoji} from './emoji.js'
 
@@ -8,14 +19,25 @@ var own = {}.hasOwnProperty
 inject(afinn165)
 inject(emoji)
 
+/**
+ * Get a polarity result from given values, optionally with one time injections.
+ *
+ * @param {Array.<string>} values
+ * @param {Inject} inject
+ * @returns {Polarity}
+ */
 export function polarity(values, inject) {
   var words = values || []
   var index = words.length === 0 ? 1 : words.length
   var positivity = 0
   var negativity = 0
+  /** @type {Array.<string>} */
   var positive = []
+  /** @type {Array.<string>} */
   var negative = []
+  /** @type {string} */
   var value
+  /** @type {number} */
   var weight
 
   while (index--) {
@@ -44,8 +66,13 @@ export function polarity(values, inject) {
   }
 }
 
-// Inject values on the `polarities` object.
+/**
+ * Inject values on the `polarities` object.
+ *
+ * @param {Inject} values
+ */
 export function inject(values) {
+  /** @type {string} */
   var value
 
   for (value in values) {
@@ -55,7 +82,12 @@ export function inject(values) {
   }
 }
 
-// Get the polarity of a word.
+/**
+ * Get the polarity of a word.
+ *
+ * @param {string} value
+ * @param {Inject} inject
+ */
 function getPolarity(value, inject) {
   if (own.call(polarities, value)) {
     return polarities[value]
